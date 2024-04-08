791fa699eb52efe3157f5bc451205fc5c834d76d
John <john.b.wilcock@gmail.com>
2024-04-08 19:14:58 +0930

    Added array contents

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 function mixedMessages()  {
-   const greetingArr = []
-   const complimentArr = []
-   const actionArr = []
+   const greetingArr = ['Fuck face', 'Monkey Butt', 'Cock bag', 'Douche canoe', 'Gob-Shite',]
+   const complimentArr = ['Beautiful', 'Strong', 'Awesome', 'Resiliant', 'Magnificent',]
+   const actionArr = ['Kick arse', 'Make magic', 'Own the day', 'Make your bed', 'Show the world whos boss',]
 
    let greeting = 'Fuck face'
    let compliment = 'super awesome'
