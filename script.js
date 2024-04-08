262527205324d5ae6d9e2af6d2c134648d2daf48
John <john.b.wilcock@gmail.com>
2024-04-08 19:04:49 +0930

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function mixedMessages()  {
+   const greetingArr = []
+   const complimentArr = []
+   const actionArr = []
+
+   let greeting = 'Fuck face'
+   let compliment = 'super awesome'
+   let action = 'kick arse'
+
+    console.log(`Good Morning ${greeting}! You are ${compliment} and it's time to ${action}!`);
+   return 
+}
+
+
+// mixedMessages();
